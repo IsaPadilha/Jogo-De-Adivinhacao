24f432214a216044ce70187e204e2e55a9ff1234
Isadora Padilha <isap4619@gmail.com>
2026-03-21 14:24:05 -0300

    Adicionado fluxo de input/output do jogo

--- JogoDeAdivinhacao.ConsoleApp1/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp1/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("-------------------");
+Console.WriteLine("Jogo de Adivinhação");
+Console.WriteLine("-------------------");
+
+Console.WriteLine();
+Console.WriteLine("Digite um número:");
+string strNumeroDigitado = Console.ReadLine();
+
+Console.WriteLine("O número digitado foi: " + strNumeroDigitado);
