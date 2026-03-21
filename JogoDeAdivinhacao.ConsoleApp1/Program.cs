1dab2a4f814c644249309cfd6fd3981a8115ca65
Isadora Padilha <isap4619@gmail.com>
2026-03-21 15:28:18 -0300

    incluido dicas sobre a proximidade do "chute" do usuário

--- JogoDeAdivinhacao.ConsoleApp1/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp1/Program.cs
@@ -1,13 +1,38 @@
 ﻿using System.Security.Cryptography;
-Console.WriteLine("-------------------");
-Console.WriteLine("Jogo de Adivinhação");
-Console.WriteLine("-------------------");
-
-Console.WriteLine();
-Console.WriteLine("Digite um número:");
-string strNumeroDigitado = Console.ReadLine();
 
 int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21); // gerar um número aleatório entre 1 e 20
 
-Console.WriteLine("O número digitado foi: " + numeroAleatorio);
-Console.ReadLine();
+bool jogoDeveContinuar = true;
+
+while (jogoDeveContinuar == true)
+{
+    Console.WriteLine("-------------------");
+    Console.WriteLine("Jogo de Adivinhação");
+    Console.WriteLine("-------------------");
+
+    Console.WriteLine();
+    Console.Write("Digite um número: ");
+    int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+    if (numeroDigitado == numeroAleatorio)
+    {
+        Console.WriteLine("Parabéns, você acertou! O número é " + numeroAleatorio);
+    }
+    else if (numeroDigitado < numeroAleatorio)
+    {
+        Console.WriteLine("O número digitado foi menor do que o número secreto!");
+    }
+    else
+    {
+        Console.WriteLine("O número digitado foi maior do que o número secreto!");
+    }
+
+    Console.WriteLine();
+    Console.Write("Deseja continuar? (s/n): ");
+    string opcaoCont = Console.ReadLine();
+
+    if (opcaoCont != "s" && opcaoCont != "S")
+    {
+        jogoDeveContinuar = false;
+    }
+}
